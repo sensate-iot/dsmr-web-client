7065470ba90075f6073db9f5a9b3c33cf17fe215
Michel Megens <michel@michelmegens.net>
2021-03-04 20:56:13 +0100

    Remove unused code.
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- SensateIoT.SmartEnergy.Dsmr.WebClient.Service/Settings/Listener.cs
+++ SensateIoT.SmartEnergy.Dsmr.WebClient.Service/Settings/Listener.cs
@@ -8,14 +8,10 @@ namespace SensateIoT.SmartEnergy.Dsmr.WebClient.Service.Settings
 		public string UserId { get; set; }
 		public string ApiKey { get; set; }
 		public IDictionary<string, Sensor> Sensors { get; }
-		//public IDictionary<string, string> GasSensorMapping { get; }
-		//public IDictionary<string, string> GasSensors { get; }
 
 		public Listener()
 		{
 			this.Sensors = new Dictionary<string, Sensor>();
-			//this.GasSensorMapping = new Dictionary<string, string>();
-			//this.GasSensors = new Dictionary<string, string>();
 		}
 	}
 }
